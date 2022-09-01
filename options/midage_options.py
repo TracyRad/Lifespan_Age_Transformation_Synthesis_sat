6535cb92e1bc7987912f8c50850944f3962a2f25
Tracy Radsvick <tracy.radsvick@stonybrook.edu>
2022-09-01 18:53:18 -0400

    Update midage_options.py

--- options/midage_options.py
+++ options/midage_options.py
@@ -5,7 +5,7 @@ import os
 from util import util
 import torch
 
-class BaseOptions():
+class AgeOptions():
     def __init__(self):
         self.parser = argparse.ArgumentParser()
         self.initialized = False
